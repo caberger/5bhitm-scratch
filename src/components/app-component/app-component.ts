921b477c08a58bfc7df742640e409a40db220bc6
caberger <c.aberger@htl-leonding.ac.at>
2024-09-26 15:27:09 +0200

    ViewModel

--- src/components/app-component/app-component.ts
+++ src/components/app-component/app-component.ts
@@ -1,19 +1,66 @@
 import { html, render } from "lit-html"
-import { store } from "../../model/model"
+import { Model, store } from "../../model/model"
+import { ToDo } from "../../model/todo"
+import { map } from "rxjs"
 
-const template = html`
-<div class="container">
-Hello World2
-</div>
-`
-
+interface RowViewModel {
+    id: number
+    text: string
+}
+interface ViewModel {
+    header: string
+    rows: RowViewModel[]
+}
 class AppComponent extends HTMLElement {
     connectedCallback() {
         console.log("App component connected")
-        store.subscribe(model => {
-            console.log(`${model.greeting} todos changed: `, model.todos)
+        store
+            .pipe(map(toViewModel))
+            .subscribe(model => {
+                const html = todoTable(model)
+                render(html, this)
         })
-        render(template, this)
     }
 }
 customElements.define("app-component", AppComponent)
+
+function toViewModel(model: Model) {
+    function totoDoModel(toDo: ToDo) {
+        const toModel: RowViewModel = {
+            id: toDo.id,
+            text: toDo.title
+        }
+        return toModel
+    }
+    const rows = model.todos.map(totoDoModel)
+    const viewModel: ViewModel = {
+        header: "Title",
+        rows
+    }
+    return viewModel
+}
+
+function todoTable(vm: ViewModel) {
+    const todoTemplate = vm.rows.map(todoRow)
+    return html`
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>${vm.header}</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${todoTemplate}
+            </tbody>
+        </table>
+    `
+}
+function todoRow(toDo: RowViewModel) {
+    return html`
+        <tr>
+            <td>${toDo.id}</td>
+            <td>${toDo.text}</td>
+        </tr>
+    `
+}
